5e6f34212efc5fe89bb025e7dc4fe7e4ae11c871
Nikhil Kumar <nikhilraj875727@gmail.com>
2026-01-05 19:54:08 +0530

    Added few components in Home page & set up express server

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Carousel from 'react-multi-carousel'
+import { bannerData } from '../constants/data';
+import { styled } from '@mui/material'
+import "react-multi-carousel/lib/styles.css";
+
+const Image = styled('img')({
+    width: '100%',
+    height: 280
+});
+
+const responsive = {
+   desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
+const Banner = () => {
+  return (
+        <Carousel 
+            responsive={responsive}
+            // showDots={true}
+            swipeable={false}
+            draggable={false}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={3000}
+            keyBoardControl={true}
+            containerClass="carousel-container"
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+        >
+                {
+                    bannerData.map(data=>(
+                        <Image src={data.url} alt="banner" />
+                    ))
+                }
+        </Carousel>
+  )
+}
+
+export default Banner
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+import NavBar from './NavBar'
+import Banner from './Banner'
+import { Box, styled } from '@mui/material'
+
+const Component = styled(Box)`
+    padding: 10px;
+    background-color: #F2F2F2;
+`
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Component>
+        <Banner />
+      </Component>
+    </>
+  )
+}
+
+export default Home
